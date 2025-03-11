c27de28ae24f758e9f61538faafe433d1adc3e27
DanielOrsovy <116282112+DanielOrsovy@users.noreply.github.com>
2025-03-11 16:19:12 +0100

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,2 +1,12 @@
 <?php
+echo '<nav id="navbar">
+    <div id="logo"><a href="index.php"> <span class="text-primary"><img src="./obrazky/logo/bwlogo.svg" alt="logo" class="logo"> Osobní portfolio</span></a></div>
+    <ul>
+        <li><a href="index.html"><i class="fa-solid fa-house"></i></a></li>
+        <li><a href="obrazy.html">Obrazy</a></li>
+        <li><a href="komiks.html?pg=1#showComic">Komiks</a></li>
+        <li><a href="objednavka.html">Objednávka</a></li>
+        <li><a href="o-me.html">O mě</a></li>
+    </ul>
+</nav>';
 ?>
